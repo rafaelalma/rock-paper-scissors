9e77843baca80c5ce74f07ab1722a1446d0194bf
Rafael Álvarez <rafaelalma92@gmail.com>
2021-11-07 12:38:16 +0100

    Finished UI feature

--- main.js
+++ main.js
@@ -1,116 +1,123 @@
-function game() {
-  const NUMBER_OF_GAMES = 5;
-  // If you reach this amount of victories you have already won
-  const necessaryVictories = Math.ceil(NUMBER_OF_GAMES / 2);
+// TODO: Refactor
+// TODO: Create HTML elements with JS in order to resemble more to a game
+const NUMBER_OF_VICTORIES = 5;
 
-  function computerPlay() {
-    // Return 0-2
-    const randomNumber = Math.floor(Math.random() * 3);
+const playerSelectionInfo = document.querySelector("#player-selection");
+const computerSelectionInfo = document.querySelector("#computer-selection");
 
-    switch (randomNumber) {
-      case 0:
-        return "rock";
-      case 1:
-        return "paper";
-      case 2:
-        return "scissors";
-      default:
-        throw new Error("Unreachable computer option");
-    }
-  }
+const roundMessageInfo = document.querySelector("#round-message");
 
-  function playRound(playerSelection, computerSelection) {
-    if (computerSelection === "rock") {
-      if (playerSelection === "rock") {
-        return "Tie! Rock and Rock";
-      } else if (playerSelection === "paper") {
-        return "You Win! Paper beats Rock";
-      } else {
-        return "You Lose! Rock beats Scissors";
-      }
-    } else if (computerSelection === "paper") {
-      if (playerSelection === "rock") {
-        return "You Lose! Paper beats Rock";
-      } else if (playerSelection === "paper") {
-        return "Tie! Paper and Paper";
-      } else {
-        return "You Win! Scissors beats Paper";
-      }
-    } else {
-      if (playerSelection === "rock") {
-        return "You Win! Rock beats Scissors";
-      } else if (playerSelection === "paper") {
-        return "You Lose! Scissors beats Paper";
-      } else {
-        return "Tie! Scissors and Scissors";
-      }
-    }
-  }
+const playerVictoriesInfo = document.querySelector("#player-victories");
+const computerVictoriesInfo = document.querySelector("#computer-victories");
 
-  /* function validatePlayerSelection(playerSelection) {
-    return (
-      playerSelection === "rock" ||
-      playerSelection === "paper" ||
-      playerSelection === "scissors"
-    );
-  } */
+const gameMessageInfo = document.querySelector("#game-message");
+const restartMessageInfo = document.querySelector("#restart-message");
 
-  let playerVictories = 0;
-  let computerVictories = 0;
+let playerVictories = 0;
+let computerVictories = 0;
 
-  // TODO: a more robust way to check the round winner
-  function checkRoundWinner(roundMessage) {
-    if (roundMessage.includes("Win")) {
-      playerVictories++;
-    } else if (roundMessage.includes("Lose")) {
-      computerVictories++;
-    }
-  }
-
-  function checkGameWinner() {
-    if (playerVictories > computerVictories) {
-      return "PLAYER WINS";
-    } else if (computerVictories > playerVictories) {
-      return "COMPUTER WINS";
-    } else {
-      return "TIE";
-    }
-  }
-
-  // TODO: while instead of for in order to avoid break keyword?
-  for (let i = 0; i < NUMBER_OF_GAMES; i++) {
-    let validPlayerSelection;
-
-    // Keeps asking until the player selection is valid
-    /* do {
-      let playerSelection = prompt(
-        "Choose Rock, Paper or Scissors: "
-      ).toLowerCase();
-
-      validPlayerSelection = validatePlayerSelection(playerSelection);
-    } while (!validPlayerSelection); */
-
+const buttons = document.querySelectorAll(".btn");
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const playerSelection = event.target.textContent.toLowerCase();
     const computerSelection = computerPlay();
 
-    console.log({ playerSelection });
-    console.log({ computerSelection });
+    playerSelectionInfo.textContent = playerSelection.toUpperCase();
+    computerSelectionInfo.textContent = computerSelection.toUpperCase();
 
     const roundMessage = playRound(playerSelection, computerSelection);
-    console.log(roundMessage);
+    roundMessageInfo.textContent = roundMessage;
 
     checkRoundWinner(roundMessage);
-    console.log({ playerVictories });
-    console.log({ computerVictories });
+    playerVictoriesInfo.textContent = playerVictories.toString();
+    computerVictoriesInfo.textContent = computerVictories.toString();
 
-    if (
-      playerVictories === necessaryVictories ||
-      computerVictories === necessaryVictories
-    ) {
-      break;
+    const maxVictories = Math.max(playerVictories, computerVictories);
+    if (maxVictories === NUMBER_OF_VICTORIES) {
+      const gameWinner = checkGameWinner();
+      gameMessageInfo.textContent = gameWinner;
+
+      buttons.forEach((button) => button.setAttribute("disabled", "disabled"));
+      setTimeout(restartGame, 5000);
+      restartMessageInfo.textContent = "Restarting...";
     }
-  }
+  });
+});
 
-  console.log(checkGameWinner());
+function computerPlay() {
+  // Return 0-2
+  const randomNumber = Math.floor(Math.random() * 3);
+
+  switch (randomNumber) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+    default:
+      throw new Error("Unreachable computer option");
+  }
 }
 
-game();
+function playRound(playerSelection, computerSelection) {
+  if (computerSelection === "rock") {
+    if (playerSelection === "rock") {
+      return "Tie! Rock and Rock";
+    } else if (playerSelection === "paper") {
+      return "You Win! Paper beats Rock";
+    } else {
+      return "You Lose! Rock beats Scissors";
+    }
+  } else if (computerSelection === "paper") {
+    if (playerSelection === "rock") {
+      return "You Lose! Paper beats Rock";
+    } else if (playerSelection === "paper") {
+      return "Tie! Paper and Paper";
+    } else {
+      return "You Win! Scissors beats Paper";
+    }
+  } else {
+    if (playerSelection === "rock") {
+      return "You Win! Rock beats Scissors";
+    } else if (playerSelection === "paper") {
+      return "You Lose! Scissors beats Paper";
+    } else {
+      return "Tie! Scissors and Scissors";
+    }
+  }
+}
+
+// TODO: a more robust way to check the round winner
+function checkRoundWinner(roundMessage) {
+  if (roundMessage.includes("Win")) {
+    playerVictories++;
+  } else if (roundMessage.includes("Lose")) {
+    computerVictories++;
+  }
+}
+
+function checkGameWinner() {
+  if (playerVictories > computerVictories) {
+    return "PLAYER WINS";
+  } else if (computerVictories > playerVictories) {
+    return "COMPUTER WINS";
+  } else {
+    return "TIE";
+  }
+}
+
+function restartGame() {
+  playerVictories = 0;
+  computerVictories = 0;
+
+  playerSelectionInfo.textContent = "";
+  computerSelectionInfo.textContent = "";
+  roundMessageInfo.textContent = "";
+  playerVictoriesInfo.textContent = playerVictories.toString();
+  computerVictoriesInfo.textContent = computerVictories.toString();
+  gameMessageInfo.textContent = "";
+  restartMessageInfo.textContent = "";
+
+  buttons.forEach((button) => button.removeAttribute("disabled"));
+}
